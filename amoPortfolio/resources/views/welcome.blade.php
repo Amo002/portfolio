6e4c6b5f1aaf6ba2ec369963c43de80b9d8ebd94
Amo002 <m.alberqdar20@gmail.com>
2024-11-26 14:43:18 +0300

    navbar style done

---
+++ amoPortfolio/resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+    <title>Mohamad Alberqdar - Amo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/base/style.css') }}">
+</head>
+<body>
+    <header>
+        <h1>Mohamad Alberqdar</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Pages</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
